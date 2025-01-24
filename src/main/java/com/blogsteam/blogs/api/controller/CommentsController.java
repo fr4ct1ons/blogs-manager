c1f0412ab690428f4eb0eff752c1806813215221
Gabriel Lucena <gabriellucena23@gmail.com>
2025-01-23 22:41:03 -0300

    Finished CommentController

--- src/main/java/com/blogsteam/blogs/api/controller/CommentsController.java
+++ src/main/java/com/blogsteam/blogs/api/controller/CommentsController.java
@@ -1,6 +1,7 @@
 package com.blogsteam.blogs.api.controller;
 
 import com.blogsteam.blogs.api.request.CreateCommentRequest;
+import com.blogsteam.blogs.api.request.UpdateCommentRequest;
 import com.blogsteam.blogs.database.repository.CommentRepository;
 import com.blogsteam.blogs.database.repository.PostRepository;
 import com.blogsteam.blogs.database.repository.UserRepository;
@@ -9,10 +10,7 @@ import com.blogsteam.blogs.database.repository.entity.PostEntity;
 import com.blogsteam.blogs.database.repository.entity.UserEntity;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.Optional;
 
@@ -58,5 +56,43 @@ public class CommentsController {
         commentRepository.saveAndFlush(entity);
 
         return ResponseEntity.ok(entity);
+    }
+
+    @PostMapping(path = "updateComment")
+    public ResponseEntity updateComment(@RequestBody UpdateCommentRequest request)
+    {
+        if(request.getNewContents().isEmpty())
+        {
+            return ResponseEntity.status(400).body("Comment contents was empty.");
+        }
+
+        Optional<CommentEntity> commentOpt = commentRepository.findById(request.getCommentId());
+
+        if(commentOpt.isEmpty())
+        {
+            return ResponseEntity.status(404).body("No comment with ID " + request.getCommentId() + " was found.");
+        }
+        CommentEntity comment = commentOpt.get();
+
+        comment.setContents(request.getNewContents());
+        commentRepository.saveAndFlush(comment);
+
+        return ResponseEntity.ok(comment);
+    }
+
+    @DeleteMapping(path = "deleteComment")
+    public ResponseEntity deleteComment(@RequestParam Integer id)
+    {
+        Optional<CommentEntity> commentOpt = commentRepository.findById(id);
+
+        if(commentOpt.isEmpty())
+        {
+            return ResponseEntity.status(404).body("No comment with ID " + id + " was found.");
+        }
+        CommentEntity comment = commentOpt.get();
+
+        commentRepository.delete(comment);
+
+        return ResponseEntity.noContent().build();
     }
 }
